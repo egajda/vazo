cdff665d29da151ce7231471ba7c656aebae9ea5
Esti <egajda@umich.edu>
2019-08-11 21:40:59 -0400

    Create basic views for users and funds

--- src/Main.elm
+++ src/Main.elm
@@ -137,89 +137,203 @@ view model =
         HomePage ->
             { title = "Vazo"
             , body =
-                [ viewHeader HomePage
-                , text (Url.toString model.url)
+                [ div [ class "layout-documentation page-columns" ]
+                    [ viewHeader HomePage model.active_nav
+                    , text (Url.toString model.url)
+                    ]
                 ]
             }
 
         FundsPage ->
             { title = "Funds"
             , body =
-                [ viewHeader FundsPage
-                , text model.url.path
+                [ div [ class "layout-documentation page-columns" ]
+                    [ viewHeader FundsPage model.active_nav
+                    , viewFunds model
+                    ]
                 ]
             }
 
         UsersPage ->
             { title = "Users"
             , body =
-                [ viewHeader UsersPage
-                , text model.url.path
+                [ div [ class "layout-documentation page-columns" ]
+                    [ viewHeader UsersPage model.active_nav
+                    , viewUsers model
+                    ]
                 ]
             }
 
 
-viewHeader : Page -> Html msg
-viewHeader page =
+viewHeader : Page -> Bool -> Html Msg
+viewHeader page active_nav =
     let
         logo =
-            a [ class "navbar-item", href "/" ] [ img [ src "/img/vazo.png", attribute "width" "72", attribute "height" "108" ] [] ]
+            a [ class "navbar-item", href "/" ]
+                [ img
+                    [ src "/img/vazo.png"
+                    , attribute "width" "72"
+                    , attribute "height" "108"
+                    ]
+                    []
+                ]
 
         navisactive =
-            case page of
-                HomePage ->
-                    False
+            active_nav
 
-                UsersPage ->
-                    True
-
-                FundsPage ->
-                    True
-
-        stuff =
-            case page of
-                FundsPage ->
-                    [ text "Funds" ]
-
-                UsersPage ->
-                    [ text "Users" ]
-
-                HomePage ->
-                    [ text "HomePage" ]
-
-        links =
-            ul []
-                [ navLink FundsPage { url = "/funds", caption = "Funds" }
-                , navLink UsersPage { url = "/users", caption = "Users" }
-                ]
-
-        navLink : Page -> { url : String, caption : String } -> Html msg
-        navLink targetPage { url, caption } =
-            li [ classList [ ( "active", page == targetPage ) ] ]
-                [ a [ href url ] [ text caption ] ]
+        navLink : String -> String -> Html msg
+        navLink name url =
+            a [ class "navbar-item", href url ] [ text name ]
     in
-    nav [ class "navbar is-info is-active", attribute "role" "navigation", attribute "aria-label" "main navigation" ]
-        [ div [ class "navbar-brand" ]
-            [ logo
-            , a [ class "navbar-burger burger is-active", attribute "role" "button", attribute "aria-label" "menu", attribute "aria-expanded" "false", attribute "data-target" "navbarBasic" ]
-                [ span [ attribute "aria-hidden" "true" ] []
-                , span [ attribute "aria-hidden" "true" ] []
-                , span [ attribute "aria-hidden" "true" ] []
-                ]
+    div [ class "container" ]
+        [ nav
+            [ class "navbar has-shadow is-spaced"
+            , attribute "role" "navigation"
+            , attribute "aria-label" "main navigation"
             ]
-        , div [ classList [ ( "navbar-menu", True ), ( "is-active", navisactive ) ], attribute "id" "navbarBasic" ]
-            [ div [ class "navbar-start" ]
-                [ a [ class "navbar-item " ] [ text "Home" ]
-                , a [ class "navbar-item" ] [ text "Funds" ]
-                , a [ class "navbar-item" ] [ text "Users" ]
+            [ div [ class "navbar-brand" ]
+                [ logo
+                , div
+                    [ class "navbar-burger burger"
+                    , onClick NavToggled
+                    , classList [ ( "is-active", navisactive ) ]
+                    , attribute "role" "button"
+                    , attribute "aria-label" "menu"
+                    , attribute "disabled" ""
+                    , attribute "aria-expanded" "false"
+                    ]
+                    [ span [ attribute "aria-hidden" "true" ] []
+                    , span [ attribute "aria-hidden" "true" ] []
+                    , span [ attribute "aria-hidden" "true" ] []
+                    ]
                 ]
-            , div [ class "navbar-end" ]
-                [ div [ class "navbar-item" ]
-                    [ div [ class "buttons" ]
-                        [ a [ class "button" ] [ text "sign up" ]
-                        , a [ class "button" ] [ text "Log in" ]
+            , div
+                [ class "navbar-menu"
+                , classList [ ( "is-active", navisactive ) ]
+                ]
+                [ div [ class "navbar-start" ]
+                    [ navLink "Funds" "/funds"
+                    , navLink "Users" "/users"
+                    ]
+                , div [ class "navbar-end" ]
+                    [ div [ class "navbar-item" ]
+                        [ div [ class "buttons" ]
+                            [ a [ class "is-primary button" ] [ text "Sign up" ]
+                            , a [ class "is-light button" ] [ text "Log in" ]
+                            ]
                         ]
                     ]
                 ]
             ]
+        ]
+
+
+viewFunds : Model -> Html Msg
+viewFunds model =
+    let
+        col obj =
+            div [ class "column is-4" ] [ obj ]
+
+        row =
+            div [ class "columns is-4" ]
+                [ col card
+                , col card
+                , col card
+                ]
+
+        uofm =
+            "https://static.timesofisrael.com/www/uploads/2018/09/michigan-e1537585558733.jpg"
+
+        greg =
+            "https://avatars1.githubusercontent.com/u/23343587?s=400&v=4"
+
+        card =
+            div [ class "card" ]
+                [ div [ class "card-image" ]
+                    [ figure [ class "image is-4by3" ]
+                        [ img
+                            [ src uofm ]
+                            []
+                        ]
+                    ]
+                , div [ class "card-content" ]
+                    [ div [ class "media" ]
+                        [ div [ class "media-left" ]
+                            [ figure [ class "image is-48x48" ] [ img [ src greg ] [] ] ]
+                        , div [ class "media-content" ]
+                            [ p [ class "title is-4" ] [ text "Greg Meyer" ]
+                            , p [ class "subtitle is-6" ] [ text "@gregjm" ]
+                            ]
+                        ]
+                    ]
+                , div [ class "content" ] [ text "Greg's college tuition (read beer money)", br [] [], time [] [ text "11:09PM - 1 Jan 2020" ] ]
+                ]
+    in
+    div
+        [ class "section" ]
+        [ h1 [ class "title" ] [ text "Funds" ]
+        , row
+        , row
+        , row
+        , row
+        ]
+
+
+viewUsers : Model -> Html Msg
+viewUsers model =
+    let
+        col obj =
+            div [ class "column is-4" ] [ obj ]
+
+        row =
+            div [ class "columns is-4" ]
+                [ col card
+                , col card
+                , col card
+                ]
+
+        greg =
+            "https://avatars1.githubusercontent.com/u/23343587?s=400&v=4"
+
+        nonlink str =
+            p [ class "subtitle is-6" ] [ text str ]
+
+        card =
+            div [ class "card" ]
+                [ a [ href "/" ]
+                    [ div [ class "card-image" ]
+                        [ figure [ class "image " ]
+                            [ img
+                                [ src greg ]
+                                []
+                            ]
+                        ]
+                    , div [ class "card-content" ]
+                        [ div [ class "media" ]
+                            [ div [ class "media-content" ]
+                                [ p [ class "title is-4" ] [ text "Greg Meyer" ]
+                                , p [ class "subtitle is-6" ] [ text "@gregjm" ]
+                                ]
+                            ]
+                        ]
+                    , div [ class "card-content" ]
+                        [ nonlink "Over 6 Million Dollars donated to the Spanish Inquistion"
+                        , p [ class "subtitle" ] [ text "Most recently funds:" ]
+                        , ul []
+                            [ li [] [ nonlink "Big Pharma" ]
+                            , li [] [ nonlink "Big Vision" ]
+                            , li [] [ nonlink "Big Death" ]
+                            ]
+                        , br [] []
+                        , time [] [ nonlink "Joined on 1 Jan 2020" ]
+                        ]
+                    ]
+                ]
+    in
+    div [ class "section" ]
+        [ h1 [ class "title" ] [ text "Users" ]
+        , row
+        , row
+        , row
+        , row
         ]
